ee67577a3698894ce6bb338dc5727944b94feb86
retamprog <retamphy2004@gmail.com>
2026-07-14 10:36:20 +0530

    Going to add the cross device google drive support to the cli app

---
+++ src/cola/drive.py
@@ -0,0 +1,6 @@
+# here I will write the drive functions to control the flow of data to my google drive for cross-device support
+# I will upload the enc vault data to google drive inorder to make it accessible in other devices
+# I will use the official google drive apis to access it and make changes to the drive
+# i will create the helper functions and later attach these in the cli.py 
+
+
